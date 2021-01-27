3902c6d0c6d0adb67ae02a404021498ceab0bfaf
VishalBhatD <60818841+vishalbhatd123456@users.noreply.github.com>
2021-01-27 19:43:26 +0530

    Create EM_Lib_Updated_Vishal.py

---
+++ EM_Lib_Updated_Vishal.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  8 11:55:53 2021
+
+@author: user
+"""
+
+from sklearn import datasets
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+
+
+iris = datasets.load_iris()
+
+x = pd.DataFrame(iris.data)
+x.columns = ['Sepal_Length','Sepal_Width','Petal_Length','Petal_Width']
+
+y = pd.DataFrame(iris.target)
+y.columns = ['Targets']
+
+plt.figure(figsize=(14,7))
+colormap = np.array(['red','lime','black'])
+plt.subplot(1,2,1)
+plt.scatter(x.Sepal_Length, x.Sepal_Width, c = colormap[y.Targets], s=40)
+plt.title('Sepal')
+plt.subplot(1,2,2)
+plt.scatter(x.Petal_Length, x.Petal_Width, c = colormap[y.Targets], s=40)
+plt.title('Petal')
+
+from sklearn.mixture import GaussianMixture;
+gmm = GaussianMixture(n_components = 3).fit(x);
+y_gmm = gmm.predict(x);
+
+plt.figure(figsize=(14,7))
+colormap = np.array(['red','lime','black'])
+plt.subplot(1,2,1)
+plt.scatter(x.Petal_Length, x.Petal_Width, c = colormap[y.Targets], s=40)
+plt.title('Real')
+plt.subplot(1,2,2)
+plt.scatter(x.Petal_Length, x.Petal_Width, c = colormap[y_gmm], s=40)
+plt.title('EM')
+
+print("accuracy_score", accuracy_score(y.Targets, y_gmm))
+print("confusion_matrix\n", confusion_matrix(y.Targets, y_gmm))
